27eda0b687fa0e61e368122c5c2623dc87a6bef4
Katie <thompsonkatherine704@gmail.com>
2022-11-06 12:00:30 -0500

    fixed sql syntax error

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS cms_db;
-CREATE DATBASE cms_db;
+CREATE DATABASE cms_db;
 USE cms_db;
 
 CREATE TABLE department(
@@ -11,7 +11,7 @@ CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
 );
@@ -21,7 +21,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
+    manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES roles(id),
     FOREIGN KEY (manager_id)
