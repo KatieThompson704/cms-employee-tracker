8a3507e31a4c5b24d4dd3f4106b99b325b91818f
Katie <thompsonkatherine704@gmail.com>
2022-11-06 11:56:48 -0500

    added console table

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 // Import and require mysql2
 const mysql = require("mysql2");
+// Import and require inquirer
 const inquirer = require("inquirer");
+// Import and require console table
+const cTable = require("console.table");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
